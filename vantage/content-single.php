8cfb38c8764c9216a82fb8c1655bacdf69fd046e
Derek <bver49@users.noreply.github.com>
2015-04-29 02:04:22 +0800

    Update content-single.php

--- vantage/content-single.php
+++ vantage/content-single.php
@@ -14,21 +14,6 @@
 
 		<?php do_action('vantage_entry_main_top') ?>
 
-		<header class="entry-header">
-
-			<?php if( has_post_thumbnail() && siteorigin_setting('blog_featured_image') ): ?>
-				<div class="entry-thumbnail"><?php the_post_thumbnail( is_active_sidebar('sidebar-1') ? 'post-thumbnail' : 'vantage-thumbnail-no-sidebar' ) ?></div>
-			<?php endif; ?>
-
-			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'vantage' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-
-			<?php if ( siteorigin_setting( 'blog_post_metadata' ) && get_post_type() == 'post' ) : ?>
-				<div class="entry-meta">
-					<?php vantage_posted_on(); ?>
-				</div><!-- .entry-meta -->
-			<?php endif; ?>
-
-		</header><!-- .entry-header -->
 
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'vantage' ) ); ?>
